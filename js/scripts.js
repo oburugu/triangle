333a3569c24d7b60deacbc6e10267b614a02cf58
beenie <oburugu@gmail.com>
2018-08-13 16:41:34 +0300

    Add all changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ function triangleTracker(){
 var a= parseInt(prompt("enter the length"));
 var b= parseInt(prompt("enter the height"));
 var c=parseInt(prompt("enter the hypotenuse"));
-if (isNan(a)===true ||(isNan(b)===true ||(isNan(c)===true|| a<=0|| b<=0|| c<=0) {
-  alert("input figures only")
+if (isNan(a)===true || isNan(b)===true || isNan(c)===true || a<=0 || b<=0 || c<=0) {
+alert("input a valid number")
 }
 else if ((a===b) &&(b==c)) {
 alert("This is an equilateral triangle");
