28ac8454a46b236102b7ba139a656644d34ff89c
beenie <oburugu@gmail.com>
2018-08-13 17:19:35 +0300

    Add button option

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
-function triangleTracker(){
+function triangleTracker () {
 var a= parseInt(prompt("enter the length"));
 var b= parseInt(prompt("enter the height"));
 var c=parseInt(prompt("enter the hypotenuse"));
-if (isNan(a)===true || isNan(b)===true || isNan(c)===true || a<=0 || b<=0 || c<=0) {
-alert("input a valid number")
-}
-else if ((a===b) &&(b==c)) {
-alert("This is an equilateral triangle");
-}
-else if ((a===b) || (b===c) || (a===c)) {
-alert("This is an isosceles triangle");
-} else if((a+b) <=c || (b+c) <=a || (a+c) <=b) {
-alert("This is not a triangle")
-} else {
-alert ("scalene")
-}
+if ((a===b) &&(b==c)) {
+  alert("This is an equilateral triangle");
+  }
+  else if ((a===b) || (b===c) || (a===c)) {
+  alert("This is an isosceles triangle");
+  } else if((a+b) <=c || (b+c) <=a || (a+c) <=b) {
+  alert("This is not a triangle")
+  } else {
+  alert ("scalene")
+  }
 }
